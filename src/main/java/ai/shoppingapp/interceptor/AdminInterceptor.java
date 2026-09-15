8bf57824a68f59dfce27513b98294c39768d2f62
naybhonekhant115-cmd <naybhonekhant115@gmail.com>
2026-09-15 12:35:29 +0630

    week1

--- src/main/java/ai/shoppingapp/interceptor/AdminInterceptor.java
+++ src/main/java/ai/shoppingapp/interceptor/AdminInterceptor.java
@@ -1,31 +1,32 @@
-//package ai.shoppingapp.interceptor;
-//
-//import org.springframework.web.servlet.HandlerInterceptor;
-//
-//import ai.shoppingapp.model.Role;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import jakarta.servlet.http.HttpSession;
-//
-//public class AdminInterceptor implements HandlerInterceptor{
-//	@Override
-//	public boolean preHandle(
-//			HttpServletRequest request,
-//			HttpServletResponse response,
-//			Object handler) throws Exception{
-//		
-//		HttpSession session = request.getSession(false);
-//		if(session == null || session.getAttribute("loggedInUser") == null) {
-//			
-//			response.sendRedirect("/login");
-//			return false;
-//			
-//		}
-//		UserDto user = (UserDto) session.getAttribute("loggedInUser");
-//		if (!(user.getRole().equals(Role.ADMIN)) {
-//			return false;
-//		}
-//		
-//		return true;
-//	}
-//}
+package ai.shoppingapp.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import ai.shoppingapp.model.Role;
+import ai.shoppingapp.model.UserDto;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class AdminInterceptor implements HandlerInterceptor{
+	@Override
+	public boolean preHandle(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			Object handler) throws Exception{
+		
+		HttpSession session = request.getSession(false);
+		if(session == null || session.getAttribute("loggedInUser") == null) {
+			
+			response.sendRedirect("/login");
+			return false;
+			
+		}
+		UserDto user = (UserDto) session.getAttribute("loggedInUser");
+		if (!(user.getRole().equals(Role.ADMIN))) {
+			return false;
+		}
+		
+		return true;
+	}
+}
