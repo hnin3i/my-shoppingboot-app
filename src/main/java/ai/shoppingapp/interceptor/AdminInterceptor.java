6270fad21913ae151f5404d80ea75d643b37aebf
naybhonekhant115-cmd <naybhonekhant115@gmail.com>
2026-09-15 22:46:22 +0630

    small fixes

--- src/main/java/ai/shoppingapp/interceptor/AdminInterceptor.java
+++ src/main/java/ai/shoppingapp/interceptor/AdminInterceptor.java
@@ -3,7 +3,7 @@ package ai.shoppingapp.interceptor;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import ai.shoppingapp.model.Role;
-import ai.shoppingapp.model.UserDto;
+import ai.shoppingapp.model.UserModel;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -22,7 +22,7 @@ public class AdminInterceptor implements HandlerInterceptor{
 			return false;
 			
 		}
-		UserDto user = (UserDto) session.getAttribute("loggedInUser");
+		UserModel user = (UserModel) session.getAttribute("loggedInUser");
 		if (!(user.getRole().equals(Role.ADMIN))) {
 			return false;
 		}
