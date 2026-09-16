d6813a6c2abb6367570d5c7eda9d37d9da7a355b
ShoonShoon <delvicavivi@gmail.com>
2026-09-16 13:58:39 +0630

    css and js file chage and homw controller too

--- src/main/java/ai/shoppingapp/config/WebConfig.java
+++ src/main/java/ai/shoppingapp/config/WebConfig.java
@@ -1,45 +1,45 @@
-package ai.shoppingapp.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import ai.shoppingapp.interceptor.AdminInterceptor;
-import ai.shoppingapp.interceptor.LoginInterceptor;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer{
-	private final LoginInterceptor loginInterceptor;
-	private final AdminInterceptor adminInterceptor;
-	
-	public WebConfig(LoginInterceptor loginInterceptor, AdminInterceptor adminInterceptor) {
-		this.loginInterceptor = loginInterceptor;
-		this.adminInterceptor = adminInterceptor;
-	}
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-
-        registry.addInterceptor(loginInterceptor)
-                .addPathPatterns(
-                		"products/**"           	
-                );
-
-        registry.addInterceptor(adminInterceptor)
-                .addPathPatterns(
-                		"admin/**"
-                );
-        
-    }
-        
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
-        registry.addResourceHandler("/images/categories/**")
-                .addResourceLocations(
-                        "file:///D:/Shopping/shoppingapp/src/main/resources/static/images/categories/"
-                );
-    }
-}
+//package ai.shoppingapp.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import ai.shoppingapp.interceptor.AdminInterceptor;
+//import ai.shoppingapp.interceptor.LoginInterceptor;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer{
+//	private final LoginInterceptor loginInterceptor;
+//	private final AdminInterceptor adminInterceptor;
+//	
+//	public WebConfig(LoginInterceptor loginInterceptor, AdminInterceptor adminInterceptor) {
+//		this.loginInterceptor = loginInterceptor;
+//		this.adminInterceptor = adminInterceptor;
+//	}
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//
+//        registry.addInterceptor(loginInterceptor)
+//                .addPathPatterns(
+//                		"products/**"           	
+//                );
+//
+//        registry.addInterceptor(adminInterceptor)
+//                .addPathPatterns(
+//                		"admin/**"
+//                );
+//        
+//    }
+//        
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//
+//        registry.addResourceHandler("/images/categories/**")
+//                .addResourceLocations(
+//                        "file:///D:/Shopping/shoppingapp/src/main/resources/static/images/categories/"
+//                );
+//    }
+//}
--- src/main/java/ai/shoppingapp/controller/HomeController.java
+++ src/main/java/ai/shoppingapp/controller/HomeController.java
@@ -1,6 +1,15 @@
 package ai.shoppingapp.controller;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+	
 }
 
