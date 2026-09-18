21f4a9279beb5e6d0406f691a4e456ab4fbdfcd9
ShoonShoon <delvicavivi@gmail.com>
2026-09-18 13:16:09 +0630

    user profile

---
+++ src/main/java/ai/shoppingapp/controller/UserController.java
@@ -0,0 +1,25 @@
+package ai.shoppingapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@GetMapping("/dashboard")
+	public String dashboard() {
+		return "user/dashboard";
+	}
+
+	@GetMapping("/profile")
+	public String profile() {
+		return "user/profile";
+	}
+
+	@GetMapping("/orders")
+	public String orders() {
+		return "user/orders";
+	}
+}
